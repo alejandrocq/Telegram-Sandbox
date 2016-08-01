21b75eeaafb874ff8cc07ca792603197c0e08b6a
Alejandro Castilla <acastillaquesada@gmail.com>
2016-08-01 11:58:11 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import org.telegram.telegrambots.TelegramBotsApi;
+
+public class Main {
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    }
+}
