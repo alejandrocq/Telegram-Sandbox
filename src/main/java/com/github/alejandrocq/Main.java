fe28077e95cd8193507e2109cb7a841f0322ac7c
Alejandro Castilla <acastillaquesada@gmail.com>
2016-08-02 13:15:05 +0200

    Added a start command. Main and handler classes have been updated with neccesary methods. The bot is not working correctly yet.

--- src/main/java/com/github/alejandrocq/Main.java
+++ src/main/java/com/github/alejandrocq/Main.java
@@ -1,15 +1,32 @@
 package com.github.alejandrocq;
 
 import com.github.alejandrocq.handlers.JavaSandboxHandler;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.logging.BotLogger;
+import org.telegram.telegrambots.logging.BotsFileHandler;
 
 public class Main {
+
+    private static final String LOGTAG = Main.class.getSimpleName();
+
     public static void main(String[] args) {
+        BotLogger.setLevel(Level.ALL);
+        BotLogger.registerLogger(new ConsoleHandler());
+        try {
+            BotLogger.registerLogger(new BotsFileHandler());
+        } catch (IOException e) {
+            BotLogger.severe(LOGTAG, e);
+        }
+
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
             telegramBotsApi.registerBot(new JavaSandboxHandler());
         } catch (TelegramApiException e) {
+            BotLogger.error(LOGTAG, e);
             e.printStackTrace();
         }
     }
--- src/main/java/com/github/alejandrocq/handlers/JavaSandboxHandler.java
+++ src/main/java/com/github/alejandrocq/handlers/JavaSandboxHandler.java
@@ -1,17 +1,38 @@
 package com.github.alejandrocq.handlers;
 
 import com.github.alejandrocq.BotConfig;
+import com.github.alejandrocq.commands.StartCommand;
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.logging.BotLogger;
 
-public class JavaSandboxHandler extends TelegramLongPollingBot {
+public class JavaSandboxHandler extends TelegramLongPollingCommandBot {
+
+    private final String LOGTAG = JavaSandboxHandler.class.getSimpleName();
+
+    public JavaSandboxHandler() {
+        register(new StartCommand());
+
+        registerDefaultAction((absSender, message) -> {
+            SendMessage commandUnknownMessage = new SendMessage();
+            commandUnknownMessage.setChatId(message.getChatId().toString());
+            commandUnknownMessage.setText("The command '" + message.getText() + "' is not known by this bot.");
+            try {
+                absSender.sendMessage(commandUnknownMessage);
+            } catch (TelegramApiException e) {
+                BotLogger.error(LOGTAG, e);
+            }
+        });
+    }
+
+    @Override public void processNonCommandUpdate(Update update) {
+        //Handle situations when the user doesn't type a command
+    }
 
     @Override public String getBotToken() {
         return BotConfig.JAVA_SANDBOX_BOT_TOKEN;
-    }
-
-    @Override public void onUpdateReceived(Update update) {
-
     }
 
     @Override public String getBotUsername() {
